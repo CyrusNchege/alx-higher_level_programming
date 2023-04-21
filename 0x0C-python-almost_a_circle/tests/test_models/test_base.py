d4a2ed1be1a5561223a2fe2beeda212dd006bc79
CyrusNchege <cyruschegecc@gmail.com>
2023-04-21 03:28:16 -0700

    Initial test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines unittests for base.py.
+    Unittest classes:
+"""
+import os
+import unittest
+
+
+class TestBase_instantiation(unittest.TestCase):
+    """unittests for testing instantiation"""
