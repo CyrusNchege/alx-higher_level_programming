1fbc153db5d6033a6ff55c133f24841835013e8c
root <cyruschegecc@gmail.com>
2023-07-25 19:34:21 +0000

    Add  script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,23 +2,9 @@
 
 
 const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
 
-function writeToFile(filePath, content) {
-  fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Successfully wrote to ${filePath}`);
-    }
-  });
-}
-
-// Command-line arguments
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-if (!filePath || !content) {
-  console.error('Usage: node 1-writeme.js <file-path> <string-to-write>');
-} else {
-  writeToFile(filePath, content);
-}
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
+});
