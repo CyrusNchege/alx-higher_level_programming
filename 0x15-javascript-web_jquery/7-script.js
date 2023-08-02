8771aac7b3bb2612f4c587ec3207eb5075611aca
CyrusNchege <cyruschegecc@gmail.com>
2023-08-03 00:43:40 +0300

    Add a jquery script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+// fetch character name from URL and display it in the HTML tag <div id="character">
+$(document).ready(function () {
+    $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (charname) {
+      $('#character').text(charname.name);
+    });
+  });
